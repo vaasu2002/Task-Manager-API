42ccb00e457b6c5066d842d34ac70ab99e74ca24
vaasu2002 <vaasu.bisht2021@vitbhopal.ac.in>
2023-07-27 15:51:19 +0530

    datafile.js

--- src/index.js
+++ src/index.js
@@ -12,6 +12,5 @@ app.use(express.urlencoded({extended: true}));
 app.use('/api', apiRoutes);
 
 app.listen(ServerConfig.PORT, () => {
-    console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
     Logger.info(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
 });
