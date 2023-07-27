34637a451425fa43de20d25bd53341bc6bfd08bc
vaasu2002 <vaasu.bisht2021@vitbhopal.ac.in>
2023-07-27 18:12:21 +0530

    delete main

--- src/utils/common/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    ErrorResponse: require('./error-response'),
-    SuccessResponse: require('./success-response'),
-    ManageFile:require('./manage-file'),
-    Enums:require('./enums')
-}
