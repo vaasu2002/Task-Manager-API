421f1f99032a65ec03188f2b10263653c590f06d
vaasu2002 <vaasu.bisht2021@vitbhopal.ac.in>
2023-07-27 15:51:30 +0530

    task controller

--- src/controllers/task-controller.js
+++ src/controllers/task-controller.js
@@ -1,37 +1,20 @@
 const { StatusCodes } = require('http-status-codes');
-<<<<<<< HEAD
 const {ManageFile} = require('../utils/common');
-=======
-const fs = require('fs');
-const path = require('path');
-const dataFilePath = path.join(__dirname,'..','tasks.json');
+const { SuccessResponse, ErrorResponse } = require('../utils/common');
+const AppError = require('../utils/errors/app-error');
+const {Enums} = require('../utils/common')
 
-const readTasks = () => {
-  const data = fs.readFileSync(dataFilePath, 'utf8');
-  return JSON.parse(data);
-}
-
-
-const saveTasks = (tasks) => {
-  const data = JSON.stringify(tasks);
-  fs.writeFile(dataFilePath, data, (err) => {
-    if (err) {
-      console.error(err);
-    } else {
-      console.log('Tasks saved successfully');
-    }
-  });
-};
->>>>>>> 9993b59d3840fe4b4eaf037bc42a76a95322f286
-  
-
+/*
+  POST /tasks
+  req-body {task_title: 'Task Title', task_description: Task Description, flag: 'PENDING'}
+*/
 const createTask = (req,res)=>{
+  try{
     const task_title = req.body.task_title;
-<<<<<<< HEAD
     const task_description = req.body.task_description;
-    const flag = req.body.flag || 'PENDING';
+    const flag = req.body.flag || Enums.FLAG_TYPES.PENDING; // default is pending
     const tasks = ManageFile.readTasks();
-    const id = tasks.length === 0 ? 1 : tasks[tasks.length - 1].id + 1;
+    const id = tasks.length === 0 ? 1 : tasks[tasks.length - 1].id + 1; // generate id
     const newTask = {
         id:id,
         task_title:task_title,
@@ -40,42 +23,137 @@ const createTask = (req,res)=>{
     };
     tasks.push(newTask);
     ManageFile.saveTasks(tasks);
-    return res.json(newTask);
+    SuccessResponse.data = newTask;
+    return res
+            .status(StatusCodes.CREATED)
+            .json(SuccessResponse);
+
+  }catch(error){
+    ErrorResponse.error = error;
+    return res  
+            .status(error.statusCode)
+            .json(ErrorResponse);
+  }
 }
 
+
+/*
+  DELETE /tasks/:id
+  req.params.id = 6
+*/
 const deleteTask = (req,res)=>{
   try{
-    const id = req.params.id;
+    const id = parseInt(req.params.id);
     let tasks = ManageFile.readTasks();
-    const taskId = tasks.findIndex((task) => task.id === id);
-    tasks.splice(taskId, 1);
+    const taskIndex  = tasks.findIndex((task) => task.id === id);
+    // Check if the task with the specified ID exists
+    if(taskIndex === -1){
+      ErrorResponse.error = new AppError('Task not found',StatusCodes.NOT_FOUND);
+      return res
+          .status(StatusCodes.NOT_FOUND)
+          .json(ErrorResponse);
+    }
+
+    tasks.splice(taskIndex , 1); // remove task 
     ManageFile.saveTasks(tasks);
-    return res.json({ message: 'Task deleted successfully' });
-  }catch(err){
-    console.error(err);
+    SuccessResponse.message = 'Tasks deleted successfully';
+    SuccessResponse.data = taskIndex ;
+    return res
+            .status(StatusCodes.OK)
+            .json(SuccessResponse);
+  }catch(error){
+    console.log(error)
+    ErrorResponse.error = error;
+    return res
+            .status(error.statusCode)
+            .json(ErrorResponse);
   }
 };
+
+
+// GET /tasks
+const getAllTasks = (req,res)=>{
+  try{
+    let tasks = ManageFile.readTasks();
+    SuccessResponse.data = tasks;
+    return res
+            .status(StatusCodes.OK)
+            .json(SuccessResponse);
+  }catch(error){
+    ErrorResponse.error = error;
+    return res
+            .status(error.statusCode)
+            .json(ErrorResponse);
+  }
+};
+
+
+/*
+  GET /tasks/:id
+  req.params.id = 6
+*/
+const getTaskById = (req,res)=>{
+  try{
+    const id = parseInt(req.params.id);
+    let tasks = ManageFile.readTasks();
+    const task = tasks.find(
+      (task) => 
+      task.id === id
+    );
+    if(!task){
+      ErrorResponse.error = new AppError('Task not found',StatusCodes.NOT_FOUND);
+      return res
+          .status(StatusCodes.NOT_FOUND)
+          .json(ErrorResponse);
+    }
+    SuccessResponse.data = task;
+    return res
+            .status(StatusCodes.OK)
+            .json(SuccessResponse);
+  }catch(error){
+    ErrorResponse.error = error;
+    return res
+            .status(error.statusCode)
+            .json(ErrorResponse);
+  }
+}
+
+
+/*
+  PUT /tasks/:id
+  req.params.id = 6
+*/
+const updateTask = (req,res)=>{
+  try{
+    const id = parseInt(req.params.id);
+    const updatedTaskData = req.body;
+    let tasks = ManageFile.readTasks();
+    const taskIndex = tasks.findIndex((task) => task.id === id);
+    if(taskIndex === -1){
+      ErrorResponse.error = new AppError('Task not found',StatusCodes.NOT_FOUND);
+      return res
+          .status(StatusCodes.NOT_FOUND)
+          .json(ErrorResponse);
+    }
+    tasks[taskIndex] = { ...tasks[taskIndex], ...updatedTaskData };
+
+    ManageFile.saveTasks(tasks);
+    SuccessResponse.data = tasks[taskIndex];
+    return res
+            .status(StatusCodes.OK)
+            .json(SuccessResponse);
+  }catch(err){
+    ErrorResponse.error = error;
+    return res
+            .status(error.statusCode)
+            .json(ErrorResponse);
+  }
+}
 
 module.exports = {
   createTask,
   deleteTask,
-=======
-    // const task_description = req.body.task_description;
-    // const flag = req.body.flag;
-    const tasks = readTasks();
-    console.log(tasks)
-    const id = tasks[tasks.length -1].id+1;
-    const newTask = {
-        id:id,
-        task_title:task_title,
-    };
-    tasks.push(newTask);
-    // saveTasks(tasks);
-    return res.json(newTask);
-}
-
-
-module.exports = {
-  createTask
->>>>>>> 9993b59d3840fe4b4eaf037bc42a76a95322f286
+  getAllTasks,
+  getTaskById,
+  updateTask
 };
